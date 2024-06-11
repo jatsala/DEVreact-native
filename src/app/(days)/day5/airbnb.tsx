3c7cfc53d1c4d789f89b9f3ef6d4930f458af226
jatsala <jatsalaberria@gmail.com>
2024-06-11 09:57:19 -0400

    Add AirBnbScreen

---
+++ src/app/(days)/day5/airbnb.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AirbnbScreen = () => {
+  return (
+    <View>
+      <Text>Airbnb</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AirbnbScreen;
